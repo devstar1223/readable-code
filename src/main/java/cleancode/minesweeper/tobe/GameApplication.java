710f4d0c6b1f04119d143cc54e20dda680e0dc7f
kangsan <devstar1223@gmail.com>
2024-10-24 22:14:17 +0900

    DIP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,17 +1,29 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.gamelevel.Advanced;
-import cleancode.minesweeper.tobe.gamelevel.GameLevel;
-import cleancode.minesweeper.tobe.gamelevel.Middle;
-import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+import cleancode.minesweeper.tobe.gamelevel.*;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
 
-        GameLevel gameLevel = new VeryBeginner();
+        GameLevel gameLevel = new Beginner();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        Minesweeper minesweeper = new Minesweeper(gameLevel,inputHandler,outputHandler);
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /**
+     * DIP (Dependency Inversion Principle)
+     *
+     * DI (Dependency Injection) - "3" (제 3자가 두 객체간 의존성을 맺어주고 주입해준다.)
+     *
+     * IoC (Inversion of Control) - 프로그램 제어의 주도권이 프레임워크에 있다. (스프링의 규격에 맞춰 코딩을 한다.)
+     *
+     */
 }
