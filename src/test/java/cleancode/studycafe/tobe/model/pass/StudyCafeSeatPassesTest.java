d9f5fe1bb45ea19beb75f0a175114154704e6e6f
kangsan <devstar1223@gmail.com>
2025-03-16 16:14:47 +0900

    test: 이용자 선택 타입 좌석 목록 확인 테스트 추가

---
+++ src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassesTest.java
@@ -0,0 +1,25 @@
+package cleancode.studycafe.tobe.model.pass;
+
+import cleancode.studycafe.tobe.io.provider.SeatPassFileReader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+class StudyCafeSeatPassesTest {
+  @DisplayName("이용자가 선택한 타입에 맞는 좌석 목록이 2개이상 있다.")
+  @Test
+  void test() {
+      //given
+      SeatPassFileReader seatPassFileReader = new SeatPassFileReader();
+      StudyCafeSeatPasses studyCafeSeatPasses = seatPassFileReader.getSeatPasses();
+
+      //when
+      List<StudyCafeSeatPass> studyCafeSeatPassList = studyCafeSeatPasses.findPassBy(StudyCafePassType.FIXED);
+
+      //then
+      assertThat(studyCafeSeatPassList).hasSizeGreaterThan(2);
+  }
+}
